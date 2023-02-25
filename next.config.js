9fec005c860a874e3fd2d2d96b799d2a53237c07
Paul Singh <hey@paulsingh.dev>
2023-02-25 15:56:50 +0000

    :sparkles: Add the language to the HTML tag

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  i18n: {
+    locales: ["en-GB"],
+    defaultLocale: "en-GB",
+  },
 };
 
 module.exports = nextConfig;
